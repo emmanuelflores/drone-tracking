5b11875a196cbbe34057b005da3696eb6378b2c7
efe <efeapple@gmail.com>
2015-02-15 09:50:53 +0100

    added radio gps files

---
+++ radio-gps/Receiver/Receiver.ino
@@ -0,0 +1,88 @@
+#include <SD.h>
+
+void setup(){
+Serial.begin(9600);
+delay(5000);
+}
+
+
+int read_first_sample()
+{
+	int samples = 0;
+	while (samples < 1) // 1 seconde wacht
+	{
+		delay(10);
+		samples = Serial.available();
+        }
+	byte read = Serial.read();
+	return read;
+}
+
+void empty()
+{
+	while (Serial.available())
+	{
+		Serial.read();
+	}
+//Serial.println("Empty");
+}
+
+
+byte sendByte(byte value)
+{
+	Serial.write(value);
+	return value;
+}
+
+void atdb()
+{
+	sendByte(0x7E);
+	sendByte(0x00);
+	sendByte(0x04);
+
+	long sum = 0;
+
+	sum += sendByte(0x08);
+	sum += sendByte(0x01);
+	sum += sendByte('D');
+	sum += sendByte('B');
+
+	sendByte( 0xFF - (sum & 0xFF));
+//       Serial.println("Send");
+	delay(10);
+}
+
+void giveRSSI()
+{
+	int start_byte = read_first_sample(); // 7E = 126
+//        Serial.println("Received");
+	if (start_byte = 126)
+	{
+		read_first_sample(); // 00
+		read_first_sample(); // 06 length
+		read_first_sample(); // 88 AT response
+		read_first_sample(); // frame ID 01
+		read_first_sample(); // 'DB'
+		read_first_sample();
+		read_first_sample(); // Ok -status
+		
+		int value = Serial.read();
+                Serial.println(value);
+	}
+	else
+	{
+	}
+	delay(10);
+}
+
+
+
+
+void loop()
+{
+	empty();
+	atdb();
+	giveRSSI();
+	delay(10);
+}
+
---
+++ radio-gps/Sender/Sender/Sender.ino
@@ -0,0 +1,58 @@
+void setup(){
+Serial.begin(9600);
+delay(5000);
+}
+
+void empty()
+{
+	while (Serial.available())
+	{
+		Serial.read();
+	}
+//Serial.println("Empty");
+}
+
+
+byte sendByte(byte value)
+{
+	Serial.write(value);
+	return value;
+}
+
+void SendRSSI()
+{
+	sendByte(0x7E);
+	sendByte(0x00);
+	sendByte(0x10);
+
+	long sum = 0;
+// 7E 00 10 17 01 00 13 A2 00 40 BE 3B 99 FF FE 02 44 30 04 E9
+	sum += sendByte(0x17);
+	sum += sendByte(0x01);
+	sum += sendByte(0x00);
+	sum += sendByte(0x13);
+	sum += sendByte(0xA2);
+	sum += sendByte(0x00);
+	sum += sendByte(0x40);
+	sum += sendByte(0xBE);
+	sum += sendByte(0x3B);
+	sum += sendByte(0x99);
+	sum += sendByte(0xFF);
+	sum += sendByte(0xFE);
+	sum += sendByte(0x02);
+	sum += sendByte(0x44);
+	sum += sendByte(0x30);
+	sum += sendByte(0x04);
+
+	sendByte( 0xFF - (sum & 0xFF));
+//       Serial.println("Send");
+	delay(200);
+}
+
+void loop()
+{
+	empty();
+	SendRSSI();
+	delay(10);
+}
+
