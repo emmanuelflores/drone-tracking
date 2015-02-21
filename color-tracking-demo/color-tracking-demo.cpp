88aaeb2583801b7ff0d36a92d011ae862baa5b10
Boaz Stolk <boaz.stolk@gmail.com>
2015-02-21 16:58:31 +0100

    add RGB to try-out

--- color-tracking-demo/color-tracking-demo.cpp
+++ color-tracking-demo/color-tracking-demo.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 // main entry point
 int main(int argc, char** argv)
@@ -19,8 +20,55 @@ int main(int argc, char** argv)
     return -1;
   }
 
+  // HSV section
   // create display window
-  cv::namedWindow("Result window", CV_WINDOW_AUTOSIZE);
+  cv::namedWindow("HSV Result window", CV_WINDOW_AUTOSIZE);
+
+  // create HSV controls window
+  cv::namedWindow("HSV Controls", CV_WINDOW_AUTOSIZE);
+
+  // HSV global values
+  int iLowH = 0;
+  int iHighH = 179;
+  int iLowS = 0;
+  int iHighS = 255;
+  int iLowV = 0;
+  int iHighV = 255;
+
+  // Hue (0 - 179)
+  cv::createTrackbar("LowH", "HSV Controls", &iLowH, 179);
+  cv::createTrackbar("HighH", "HSV Controls", &iHighH, 179);
+  // Saturation (0 - 255)
+  cv::createTrackbar("LowS", "HSV Controls", &iLowS, 255);
+  cv::createTrackbar("HighS", "HSV Controls", &iHighS, 255);
+  // Value (0 - 255)
+  cv::createTrackbar("LowV", "HSV Controls", &iLowV, 255);
+  cv::createTrackbar("HighV", "HSV Controls", &iHighV, 255);
+
+  // create display window
+  cv::namedWindow("RGB Result window", CV_WINDOW_AUTOSIZE);
+
+  // RGB section
+  // create RGB controls window
+  cv::namedWindow("RGB Controls", CV_WINDOW_AUTOSIZE);
+
+  // RGB global values
+  int iLowR = 0;
+  int iHighR = 255;
+  int iLowG = 0;
+  int iHighG = 255;
+  int iLowB = 0;
+  int iHighB = 255;
+
+  // Red (0 - 255)
+  cv::createTrackbar("LowR", "RGB Controls", &iLowR, 255);
+  cv::createTrackbar("HighR", "RGB Controls", &iHighR, 255);
+  // Green (0 - 255)
+  cv::createTrackbar("LowG", "RGB Controls", &iLowG, 255);
+  cv::createTrackbar("HighG", "RGB Controls", &iHighG, 255);
+  // Blue (0 - 255)
+  cv::createTrackbar("LowB", "RGB Controls", &iLowB, 255);
+  cv::createTrackbar("HighB", "RGB Controls", &iHighB, 255);
 
   for(;;)
   {
@@ -28,8 +76,35 @@ int main(int argc, char** argv)
     // grab frame
     cap >> frame;
 
-    // show frame
-    cv::imshow("Result window", frame);
+    { // HSV try-out
+      // convert to HSV
+      cv::Mat imgHSV;
+      cv::cvtColor(frame, imgHSV, cv::COLOR_BGR2HSV);
+
+      // thresholded image
+      cv::Mat imgThresholded;
+      cv::inRange(
+        imgHSV,
+        cv::Scalar(iLowH, iLowS, iLowV),
+        cv::Scalar(iHighH, iHighS, iHighV),
+        imgThresholded);
+
+      // show frame
+      cv::imshow("HSV Result window", imgThresholded);
+    }
+
+    { // RGB try-out
+      // thresholded image
+      cv::Mat imgThresholded;
+      cv::inRange(
+        frame,
+        cv::Scalar(iLowB, iLowG, iLowR),
+        cv::Scalar(iHighB, iHighG, iHighR),
+        imgThresholded);
+
+      // show frame
+      cv::imshow("RGB Result window", imgThresholded);
+    }
 
     // give imshow some time to show the result
     // so have a 1 ms delay
